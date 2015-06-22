c7651495358fd59dfda7d60403fb704deac3b80b
vendethiel <vendethiel@hotmail.fr>
2015-06-22 11:58:31 +0200

    Now working! Only the basic stuff tho (no [],)
    Introduced the data pointer (I have no idea why I only had one ip???)
    
    I seriously need to refactor this...

--- brainfuck.rkt
+++ brainfuck.rkt
@@ -9,7 +9,7 @@
       body ...
       (loop))))
 
-(define-syntax-rule (lazy-const body ...)
+(define-syntax-rule (once body ...)
   (let ([const-result '()]
         [inited #f])
     (lambda ()
@@ -18,38 +18,55 @@
         (set! const-result (begin body ...)))
       const-result)))
 
+(define-syntax-rule (lazy-const expr)
+  (lambda _ expr))
+
 (define (vector-apply vec idx fn)
-  (vector-set! (fn (vector-ref vec idx))))
+  (vector-set! vec idx (fn (vector-ref vec idx)))
+  vec)
 
 (define (read-and-run filename)
-  (unless (file-exists? filename)
-    (raise (string-append "Cannot evaluate " filename ": no such file or directory")))
-  (define instructions (file->string filename)))
+  (if (file-exists? filename)
+      (run (file->string filename))
+      (raise (string-append "Cannot evaluate " filename ": no such file or directory"))))
+
+(define (display-and-flush . strs)
+  (display (apply string-append strs))
+  (flush-output))
 
 (define (run instructions)
   (define instructions-count (string-length instructions))
+  ; TODO I *really* need to find a way to "update" an immutable vector (copy with set in the process...)
   (let run ([memory (make-vector +size+)]
             [ip 0]
+            [dp 0]
             [jumps '()])
-    (let ([cur (lambda () (vector-ref memory ip))]
-          [change-cur (lambda (fn) (run memory (vector-apply memory ip fn) jumps))]
-          [move-ip (lambda (n) (run memory (+ ip n) jumps))])
-      (display "hi")
+    ; TODO I definitely need to move this named let arguments into
+    ;      a struct... (or a class)
+    ;      (the functions can be free ones, or for a class, ... methods)
+    (let* ([cur (lambda () (vector-ref memory dp))]
+           [apply-cur (lambda (fn) (run (vector-apply memory dp fn)
+                                    (+ ip 1) dp jumps))]
+           [move-ip (lambda (n) (run memory (+ ip n) dp jumps))]
+           [next (lambda () (move-ip 1))]
+           [move-dp (lambda (n) (run memory (+ ip 1) (+ dp n) jumps))])
       (when (not (= ip instructions-count))
         (match (string-ref instructions ip)
-          [#\< (move-ip +1)]
-          [#\> (move-ip -1)]
-          [#\+ (change-cur (curry + 1))]
-          [#\- (change-cur (curry - 1))]
-          [#\. (display (cur))]
-          [#\, (change-cur (lambda (_) (read-byte)))]
+          [#\> (move-dp +1)]
+          [#\< (move-dp -1)]
+          [#\+ (apply-cur (curry + 1))]
+          [#\- (apply-cur (curry - 1))]
+          [#\. (begin
+                 (display-and-flush (number->string (cur)))
+                 (next))]
+          [#\, (apply-cur (lazy-const (read-byte)))]
          ;[#\[ (if (= (cur) 0) forward-to-next-command )]
          ;[#\] ()]
-          [else (run memory (+ ip 1) jumps)])))))
+          [else (next)])))))
 
 ;; cli
 (command-line
   #:program "Brainket"
   #:args (filename)
 
-  (compile-and-run filename))
+  (read-and-run filename))

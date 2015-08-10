efea1bfa4b25b73d765f269813310205ba73f5ef
vendethiel <vendethiel@hotmail.fr>
2015-08-10 18:16:28 +0200

    [ and ] now work! only need to clean up the code

--- brainfuck.rkt
+++ brainfuck.rkt
@@ -18,10 +18,10 @@
 
     (super-new)
 
+    (define instructions-length (string-length instructions))
     (define memory (make-vector size))
     (define ip 0)
     (define dp 0)
-    (define jumps '())
 
     (define/public (cur)
       (vector-ref memory dp))
@@ -29,20 +29,36 @@
     (define/public (cur-instruction)
       (string-ref instructions ip))
 
+    (define/public (done?)
+      (>= ip instructions-length))
+
     (define/public (set-cur! value)
       (vector-set! memory dp value))
 
     (define/public (mutate-cur! fn)
       (vector-mutate! memory dp fn))
 
-    (define/public (move-ip n)
-      (set! ip (+ ip n)))
+    (define/public (move-ip n) (set! ip (+ ip n)))
 
-    (define/public (next)
-      (move-ip 1))
+    (define/public (next) (move-ip 1))
 
-    (define/public (move-dp n)
-      (set! dp (+ dp n)))
+    (define/public (prev) (move-ip -1))
+
+    (define/public (move-dp n) (set! dp (+ dp n)))
+
+    (define/public (go-to ip-mod char #:opposite [opposite #f])
+      (define char-stack 0)
+      (let loop ()
+        (move-ip ip-mod)
+        (unless (done?)
+          (if (eq? char (cur-instruction))
+              (when (> 0 char-stack)
+                (set! char-stack (- 1 char-stack))
+                (loop))
+              (begin
+                (when (eq? opposite (cur-instruction))
+                  (set! char-stack (+ 1 char-stack)))
+                (loop))))))
 
     (define-getter ip dp)))
 
@@ -51,18 +67,25 @@
   (define instructions-count (string-length instructions))
   ; TODO I *really* need to find a way to "update" an immutable vector
   ;  (copy with set in the process...)
-  (while (not (= (send vm get-ip) instructions-count))
+  (while (not (send vm done?))
     (match (send vm cur-instruction)
-      [#\> (send vm move-dp +1)              (send vm next)]
-      [#\< (send vm move-dp -1)              (send vm next)]
-      [#\+ (send vm mutate-cur! (curry + 1)) (send vm next)]
-      [#\- (send vm mutate-cur! (curry - 1)) (send vm next)]
-      [#\, (send vm set-cur! (read-byte))    (send vm next)]
+      [#\> (send vm move-dp +1)                  (send vm next)]
+      [#\< (send vm move-dp -1)                  (send vm next)]
+      [#\+ (send vm mutate-cur! (lambda (n) (+ n 1))) (send vm next)]
+      [#\- (send vm mutate-cur! (lambda (n) (- n 1))) (send vm next)]
+      [#\,
+       ; TODO flush input port (to remove the #\newline)
+       (send vm set-cur! (read-byte))
+       (send vm next)]
       [#\. (begin
-             (display-and-flush (number->string (send vm cur)))
+             (display-and-flush (string (integer->char  (send vm cur))))
              (send vm next))]
-      ;[#\[ (if (= (cur) 0) forward-to-next-command )]
-      ;[#\] ()]
+      [#\[ (when (= (send vm cur) 0)
+             (send vm go-to +1 #\] #:opposite #\[))
+           (send vm next)] ; skip anyway
+      [#\] (if (not (= (send vm cur) 0))
+               (send vm go-to -1 #\[ #:opposite #\])
+               (send vm next))]
       [else (send vm next)])))
 
 ;; cli
--- helpers.rkt
+++ helpers.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require (for-syntax racket/syntax))
 
-(provide while once define-getter)
+(provide while until once define-getter)
 
 (provide/contract [vector-mutate!
                    (vector? integer? (any/c . -> . any/c) . -> . any/c)]
@@ -13,6 +13,10 @@
     (when condition
       body ...
       (loop))))
+
+(define-syntax-rule (until condition body ...)
+  (while (not condition)
+    body ...))
 
 (define-syntax-rule (once body ...)
   (let ([const-result '()]
@@ -34,6 +38,23 @@
            (define/public (getter-name)
              id0)
            (define-getter id ...)))]))
+
+(define-syntax (define-setter stx)
+  (syntax-case stx ()
+    [(define-setter) #'(begin)]
+    [(define-setter id0 id ...)
+     (with-syntax ([setter-name
+                    (format-id #'id0 #:source #'id0
+                               "set-~a!" (syntax-e #'id0))])
+       #'(begin
+           (define/public (setter-name)
+             id0)
+           (define-setter id ...)))]))
+
+(define-syntax-rule (define-getter-setter id ...)
+  (begin
+    (define-getter id ...)
+    (define-setter id ...)))
 
 (define (vector-mutate! vec idx fn)
   (vector-set! vec idx (fn (vector-ref vec idx)))
